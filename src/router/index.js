6a3e3af74bd0410231a841e36309ace4345add48
jorgedemaria <jorgedemaria@pm.me>
2026-09-07 01:39:56 -0300

    Agregar títulos dinámicos por ruta

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,18 @@ import Portafolio from '../views/Portafolio.vue'
 import Contacto from '../views/Contacto.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/portafolio', component: Portafolio },
-  { path: '/contacto', component: Contacto }
+  { path: '/', component: Home, meta: { title: 'polett villarroel — arquitecta' } },
+  { path: '/portafolio', component: Portafolio, meta: { title: 'portafolio — polett villarroel' } },
+  { path: '/contacto', component: Contacto, meta: { title: 'contacto — polett villarroel' } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'Polett Villarroel'
 })
 
 export default router
